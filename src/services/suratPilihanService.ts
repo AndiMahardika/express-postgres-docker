eb1a215965eff8109ebd31d3e5241277af1b8362
AndiMahardika <andi.mahardika.mulya@gmail.com>
2025-10-24 13:52:10 +0700

    delete: file not used

--- src/services/suratPilihanService.ts
+++
@@ -1,7 +0,0 @@
-import * as suratPilihanRepo from '../repositories/suratPilihanRepo';
-
-
-export const createSuratPilihan = suratPilihanRepo.createSuratPilihan;
-export const getAllSuratPilihan = suratPilihanRepo.getAllSuratPilihan;
-export const updateSuratPilihan = suratPilihanRepo.updateSuratPilihan;
-export const deleteSuratPilihan = suratPilihanRepo.deleteSuratPilihan;
